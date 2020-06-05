298d9898b26233fe2084e91b8fbcf797901a3ab6
Renay Earnshaw <renayearnshaw@gmail.com>
2020-06-05 08:51:07 +0100

    Changing the data with a map operation

--- src/SuperIterable.java
+++ src/SuperIterable.java
@@ -2,11 +2,11 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
-import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Predicate;
 
 // A wrapper around data (an Iterable) that allows us to apply
-// operations to it (in this case, a filter operation). Each
+// operations to it (in this case, a filter or map operation). Each
 // operation returns a new wrapper object that contains modified data.
 // This will allow us to chain operations and create a pipeline.
 public class SuperIterable<E> implements Iterable<E> {
@@ -15,6 +15,15 @@ public class SuperIterable<E> implements Iterable<E> {
 
     public SuperIterable(Iterable<E> self) {
         this.self = self;
+    }
+
+    // Return a new SuperIterable containing objects that have been changed/mapped by the operation
+    public <F> SuperIterable<F> map(Function<E, F> operation) {
+        List<F> results = new ArrayList<>();
+
+        self.forEach(e -> results.add(operation.apply(e)));
+
+        return new SuperIterable<>(results);
     }
 
     // Return a new SuperIterable containing objects that pass the predicate
@@ -51,5 +60,33 @@ public class SuperIterable<E> implements Iterable<E> {
         // Check that the original list is unchanged
         System.out.println("----------------------------------------------------------------");
         strings.forEach(s -> System.out.println("> " + s));
+
+        System.out.println("----------------------------------------------------------------");
+        // Chain operations without creating intermediate variables
+        strings.filter(s -> Character.isUpperCase(s.charAt(0)))
+            // Map from a mixed case string to an upper case string
+            .map(s -> s.toUpperCase())
+            .forEach(s -> System.out.println("> " + s));
+
+        // Check that the original list is unchanged
+        System.out.println("----------------------------------------------------------------");
+        strings.forEach(s -> System.out.println("> " + s));
+
+        SuperIterable<Car> cars = new SuperIterable<>(
+            Arrays.asList(
+                Car.withGasColourPassengers(6, "Red", "Fred", "Jim", "Sheila"),
+                Car.withGasColourPassengers(3, "Octarine", "Rincewind", "Ridcully"),
+                Car.withGasColourPassengers(9, "Black", "Weatherwax", "Magrat"),
+                Car.withGasColourPassengers(7, "Green", "Valentine", "Gillian", "Anne", "Dr. Mahmoud"),
+                Car.withGasColourPassengers(6, "Red", "Ender", "Hyrum", "Locke", "Bonzo")
+        ));
+
+        System.out.println("----------------------------------------------------------------");
+        // Filter out cars that have fuel levels of six or less
+        cars.filter(car -> car.getGasLevel() > 6)
+            // Map from a Car to a String - a description of the car
+            .map(car -> car.getPassengers().get(0) + " is driving a " + car.getColour()
+                + " car with lots of fuel")
+            .forEach(description -> System.out.println("> " + description));
     }
 }
