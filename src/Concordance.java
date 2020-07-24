025f7ea7cc9caf5fb866a68a414d76fbcf51554c
Renay Earnshaw <renayearnshaw@gmail.com>
2020-07-24 08:39:59 +0100

    My final attempt at a "concordance"

--- src/Concordance.java
+++ src/Concordance.java
@@ -2,9 +2,12 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Comparator;
+import java.util.Map;
+import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.regex.Pattern;
-import java.util.stream.Stream;
+import java.util.stream.Collectors;
 
 // Build a 'Concordance' - something that lists the frequency of words in a text file
 public class Concordance {
@@ -13,23 +16,34 @@ public class Concordance {
             // Get the path to the file we want to read
             Path path = Paths.get("PrideAndPrejudice.txt");
 
-            // Read the text file as a stream and count the number of lines it contains
-            Stream<String> streamOfLines = Files.lines(path);
-            long noOfLines = streamOfLines.count();
-            System.out.println("No of lines in " + path.toString() + " is " + noOfLines);
-            System.out.println("-------------------------------------------");
-
-            // Read the text file as a stream and count the number of words it contains
+            // Build a regular expression that matches one or more non-word characters
             Pattern pattern = Pattern.compile("\\W+");
+            // Build a filter that removes empty strings
             Predicate<String> isEmpty = String::isEmpty;
             Predicate<String> notEmpty = isEmpty.negate();
-            streamOfLines = Files.lines(path)
+
+            // Read the text file as a stream of strings
+            Map<String, Long> countOfWords = Files.lines(path)
                 // map each line to a stream of the words it contains
-                .flatMap(line -> pattern.splitAsStream(line));
-            // Count the number of words in the stream, but ignore any empty words that result from
-            // the regular expression not dealing with paragraph indentations correctly
-            long noOfWords = streamOfLines.filter(notEmpty).count();
-            System.out.println("No of words in " + path.toString() + " is " + noOfWords);
+                .flatMap(line -> pattern.splitAsStream(line))
+                // ignore any empty strings that result from the regular expression including paragraph indentations
+                .filter(notEmpty)
+                // Use a Collector object to group words according to...
+                .collect(Collectors.groupingBy(
+                    // the word itself...
+                    Function.identity(),
+                    // and the number of times it occurs
+                    Collectors.counting()));
+            // Build a comparator that sorts the map created above in descending order.
+            // Use the factory method in Map.Entry for comparing by value
+            Comparator<Map.Entry<String,Long>> comparator = Map.Entry.comparingByValue();
+            // Reverse the order of the comparator
+            comparator = comparator.reversed();
+            countOfWords.entrySet().stream()
+                // Sort by the count of words
+                .sorted(comparator)
+                .limit(200)
+                .forEach(entry -> System.out.println(entry.getKey() + " is found " + entry.getValue() + " times."));
         } catch (IOException e) {
             e.printStackTrace();
         }
