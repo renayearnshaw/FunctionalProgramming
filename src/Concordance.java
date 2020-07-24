631f9fe162155e496dda8f93f629b8ce8bb47d65
Renay Earnshaw <renayearnshaw@gmail.com>
2020-07-24 09:04:24 +0100

    Improvements to Concordance class

--- src/Concordance.java
+++ src/Concordance.java
@@ -1,6 +1,5 @@
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Comparator;
 import java.util.Map;
@@ -13,37 +12,40 @@ import java.util.stream.Collectors;
 public class Concordance {
     public static void main(String[] args) {
         try {
-            // Get the path to the file we want to read
-            Path path = Paths.get("PrideAndPrejudice.txt");
-
             // Build a regular expression that matches one or more non-word characters
             Pattern pattern = Pattern.compile("\\W+");
             // Build a filter that removes empty strings
             Predicate<String> isEmpty = String::isEmpty;
             Predicate<String> notEmpty = isEmpty.negate();
 
-            // Read the text file as a stream of strings
-            Map<String, Long> countOfWords = Files.lines(path)
-                // map each line to a stream of the words it contains
+            // Build a map of the words in a text file and the number of occurrences of that word
+            Map<String, Long> countOfWords =
+                // Read a text file as a stream of strings
+                Files.lines(Paths.get("PrideAndPrejudice.txt"))
+                // Map each line to a stream of the words it contains
                 .flatMap(line -> pattern.splitAsStream(line))
-                // ignore any empty strings that result from the regular expression including paragraph indentations
+                // Ignore any empty strings that result from the regular expression including paragraph indentations
                 .filter(notEmpty)
+                // Words with different cases count as the same word
+                .map(word -> word.toLowerCase())
                 // Use a Collector object to group words according to...
                 .collect(Collectors.groupingBy(
                     // the word itself...
                     Function.identity(),
                     // and the number of times it occurs
                     Collectors.counting()));
+
             // Build a comparator that sorts the map created above in descending order.
             // Use the factory method in Map.Entry for comparing by value
-            Comparator<Map.Entry<String,Long>> comparator = Map.Entry.comparingByValue();
+            Comparator<Map.Entry<String,Long>> orderByValue = Map.Entry.comparingByValue();
             // Reverse the order of the comparator
-            comparator = comparator.reversed();
+            Comparator<Map.Entry<String,Long>> reverseOrderByValue  = orderByValue.reversed();
+
             countOfWords.entrySet().stream()
                 // Sort by the count of words
-                .sorted(comparator)
+                .sorted(reverseOrderByValue)
                 .limit(200)
-                .forEach(entry -> System.out.println(entry.getKey() + " is found " + entry.getValue() + " times."));
+                .forEach(entry -> System.out.printf("%20s: %d\n", entry.getKey(), entry.getValue()));
         } catch (IOException e) {
             e.printStackTrace();
         }
